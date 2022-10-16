2dbfee3ff4737a0fe98ed6bdc479cdecca0dd2c4
unknown <ochester4@gmail.com>
2022-10-16 17:48:09 +0100

    More Changes to js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,76 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    // MAIN SLIDER
-    $(".owl-carousel.site-slider__slides").owlCarousel({
+document.addEventListener("DOMContentLoaded", function(){
+    // OWLSLIDER
+    $(".owl-carousel").owlCarousel({
         loop:true,
-        margin: 0,
+        margin: 30,
         nav: false,
         responsiveClass: true,
         autoplay: true,
-        autoplayTimeout: 12000,
-        items: 1
+        responsive:{
+            0:{
+                items: 1
+            },
+            950:{
+                items: 2
+            }
+        }
+    });
+    // SITE FUNCTIONS
+    const toggleScroll = () =>{
+         var body = document.querySelector("body");
+         if(body.classList.contains("no-scroll")){
+            body.classList.remove("no-scroll");
+         }else{
+            body.classList.add("no-scroll");
+         }
+    }
+
+    const toggleSearchModal = () =>{
+        var searchModal = document.getElementById("searchbox");
+       
+        if(searchModal.classList.contains("open")){
+            searchModal.classList.remove("open");
+            toggleScroll();
+        }else{
+            searchModal.classList.add("open");
+            toggleScroll();
+        }
+    }
+
+    const toggleMobileMenu = () =>{
+        var mobileMenu = document.getElementById("mobilemenu");
+        if(mobileMenu.classList.contains("open")){
+            mobileMenu.classList.remove("open");
+            toggleScroll();
+        }else{
+            mobileMenu.classList.add("open");
+            toggleScroll();
+        }
+    }
+
+    // SEARCH MODAL
+    var openSearchBtn = document.getElementById("open-search");
+    var closeSearchBtn = document.getElementById("close-search")
+
+    openSearchBtn.addEventListener("click",()=>{
+        toggleSearchModal();
+    });
+    closeSearchBtn.addEventListener("click",()=>{
+        toggleSearchModal();
     });
 
-    // WIDGET SLIDER
-    $(".owl-carousel.latest-posts__list").owlCarousel({
-        loop:true,
-        margin: 0,
-        nav: false,
-        responsiveClass: true,
-        autoplay: false,
-        autoplayTimeout: 6000,
-        items: 1
-    });
+    // MOBILE MENU
+    var openMenu = document.getElementById("open-menu");
+    var closeMenu = document.getElementById("close-menu");
+    var menuOverlay = document.getElementById("menu-overlay");
 
+    openMenu.addEventListener("click", ()=>{
+        toggleMobileMenu();
+    });
+    closeMenu.addEventListener("click", ()=>{
+        toggleMobileMenu();
+    });
+    menuOverlay.addEventListener("click", ()=>{
+        toggleMobileMenu();
+    })
 });
